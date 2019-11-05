70953922d8324e5cb428b11edf146074e299c442
Eirik Mildestveit Hammerstad <eirik.mildestveit.hammerstad@nhn.no>
2019-11-05 07:47:53 +0100

    Removed relative path handling from scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 set -e
-
-# Ensure we are running from the same directory as the script, due to relative paths
-cd "$(dirname "$0")" 
-
-docker build -t hammerstad/shushbot:latest -t hammerstad/shushbot:$(date +%Y%m%d)-$(date +%H%M%S) ../ShushBot/ -f ../ShushBot/DockerFile
+docker build -t hammerstad/shushbot:latest -t hammerstad/shushbot:$(date +%Y%m%d)-$(date +%H%M%S) ShushBot -f ShushBot/Dockerfile
